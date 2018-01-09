0380bf60506d1e5bdee624eeb1417de751d7d79b
Tuomas Nylund <tunylund@gmail.com>
2018-01-09 21:57:48 +0000

    update the index page

--- public/js/lobby.js
+++ public/js/lobby.js
@@ -1,52 +1,48 @@
-$(function() {
+(function() {
 
-  var startTime = null,
-    runtimeInterval = null;
+  function updateGameTime (startTime) {
+    let runtime = new Date().getTime() - startTime,
+        days = Math.floor(runtime / 1000 / 60 / 60 / 24, 10),
+        hours = Math.floor(runtime / 1000 / 60 / 60 % 24),
+        minutes = Math.floor(runtime / 1000 / 60 % 60),
+        seconds = Math.floor(runtime / 1000 % 60)
+
+    let d = days > 0 ? days + ' days ' : '',
+        h = hours > 0 ? hours + 'h ' : ''
+
+    document.querySelector('#runtime').innerHTML = `${d}${h}${minutes}min ${seconds}s`
+  }
+
+  function joinGame () {
+    localStorage.setItem('character', JSON.stringify({
+      characterType: document.querySelector('[name=type]:checked').value,
+      name: document.querySelector('#name').value || 'herra huu'
+    }))
     
-  $.getJSON("/status", function(response) {
+    window.location = 'game.html'
+  }
 
-      var data = response;
-      $("#rooms").html(data.rooms);
-      $("#players").html(data.players);
-      startTime = data.startTime;
-      runtimeInterval = setInterval(function() {
-      
-         var now = new Date().getTime(),
-           runtime = now - startTime,
-           days = Math.floor(runtime / 1000 / 60 / 60 / 24, 10),
-           hours = Math.floor(runtime / 1000 / 60 / 60 % 24),
-           minutes = Math.floor(runtime / 1000 / 60 % 60),
-           seconds = Math.floor(runtime / 1000 % 60);
-
-      $("#runtime").html(
-        (days > 0 ? days + " days " : "") 
-        + (hours > 0 ? hours + "h " : "" )
-        + minutes + "min " + seconds + "s");
-      }, 1000);
-    
+  fetch('/status')
+    .then(res => res.json())
+    .then(status => {
+      document.querySelector('#rooms').html(status.rooms)
+      document.querySelector('#players').html(status.players)
+      setInterval(() => updateGameTime(status.startTime), 1000)
+    })
+    .catch(err => {
+      document.querySelector('#rooms').innerHTML = 0
+      document.querySelector('#players').innerHTML = 0
+      document.querySelector('#runtime').innerHTML = 'Server is down at the moment'
+    })
   
-  }).error(function() {
-      $("#rooms").html(0);
-      $("#players").html(0);
-      $("#runtime").html("Server is down at the moment");
-  });
-  
-  $("#join").click(function() {
-    
-    localStorage.setItem("character", JSON.stringify({
-      characterType: $("[name=type]:checked").val(),
-      name: "herra huu",
-    }));
-    
-    window.location = "game.html"
-  });
+  document.querySelector('form').addEventListener('submit', joinGame)
   
   try {
-    var c = JSON.parse(localStorage.getItem("character"));
-    $("#type").val(c.characterType);
-    $("#name").val(c.name);
+    const c = JSON.parse(localStorage.getItem("character"))
+    document.querySelector(`[value=${c.characterType}]`).checked = true
+    document.querySelector('#name').value = c.name
   } catch (e) {
     console.info("no old credentials found");
   }
 
-});
+})()
